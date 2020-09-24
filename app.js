e9042921f9f58ef06747d8e08fa9f0d40046c01c
superhackerboy <mark@superhackerman.com>
2020-09-24 16:24:30 -0400

    added form

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("test");
+const form = document.querySelector("form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // secret message
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  // share link
+  const link = document.querySelector("#link-input");
+  link.value = encrypted;
+});
