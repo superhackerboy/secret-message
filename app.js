cf4acaa868877fed51949f31d363907df1d06da4
superhackerboy <mark@superhackerman.com>
2020-09-25 14:22:33 -0400

    done

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
+
+if (message) {
+  const messageForm = document.querySelector("#message-form");
+  messageForm.classList.toggle("hide");
+
+  const messageShow = document.querySelector("#message-show");
+  messageShow.classList.toggle("hide");
+
+  const h1 = document.querySelector("h1");
+  h1.innerHTML = message;
+}
+
 const form = document.querySelector("form");
 form.addEventListener("submit", e => {
   e.preventDefault();
+
+  // hides and shows forms
+  const panels = document.querySelectorAll(".card-panel");
+  for (const panel of panels) {
+    panel.classList.toggle("hide");
+  }
 
   // secret message
   const input = document.querySelector("#message-input");
@@ -8,5 +28,7 @@ form.addEventListener("submit", e => {
 
   // share link
   const link = document.querySelector("#link-input");
-  link.value = encrypted;
+  link.value = `${window.location}#${encrypted}`;
+  // auto highlights link on creation
+  link.select();
 });
